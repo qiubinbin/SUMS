e71602979aaf279d9370ccf77d9f95d78e4385f0
qiubinbin <qiubin2029249991>
2020-05-19 17:07:49 +0800

    2020年5月19日 17:07:42

--- part01/forms.py
+++ part01/forms.py
@@ -3,11 +3,9 @@ from django.contrib import auth
 
 
 class LoginForm(forms.Form):
-	username = forms.CharField(label='用户名',
-	                           widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入用户名'}))
-	password = forms.CharField(label='密码',
-	                           widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '请输入密码'}))
-
+	username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入用户名'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '请输入密码'}))
+	
 	def clean(self):
 		"""数据验证"""
 		username = self.cleaned_data['username']
